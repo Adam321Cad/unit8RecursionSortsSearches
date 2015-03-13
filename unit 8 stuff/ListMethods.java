7a62b3ec968ff893277d7d9f884c78f8089dbfc3
Adam321Cad <aratoad@gmail.com>
2015-03-13 10:27:48 -0500

    dawd
    dsaf

---
+++ unit 8 stuff/ListMethods.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ListMethods
+{
+   public static ArrayList makeList(int n)
+   {
+      ArrayList tempList = null;
+      if (n <= 0)  // The smallest list we can make
+      {
+
+
+
+      }
+      else        // All other size lists are created here
+      {
+
+
+
+      }
+      return tempList;
+   }
+}
