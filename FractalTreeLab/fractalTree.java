5f8295bbe4e5103bedbb823c1e3bb3bc03b7e2c7
Adam321Cad <aratoad@gmail.com>
2015-03-26 11:27:41 -0500

    ytkfd
    tje

--- FractalTreeLab/fractalTree.java
+++ FractalTreeLab/fractalTree.java
@@ -4,12 +4,13 @@ import javax.swing.JPanel;
 public class fractalTree extends JPanel
 {
     private final double angle1 = Math.PI/6;
-    private int length = 100;
-    private final int decrease =-10;
+    //private int length = 100;
+    private final int decrease =-5;
     int oldx = 500;
     int oldy = 500;
     int newx = 0;
     int newy = 0;
+    int count = 0;
     //private final int shortening;
     /** description of instance variable x (add comment for each instance variable) */
 
@@ -22,19 +23,41 @@ public class fractalTree extends JPanel
     {
         super.paintComponent (g);
         g.setColor (Color.green);
-        g.drawLine(500,600,500,500);
-        for(int i = 0; i<10; i++){
-        angleCalc(g,angle1);
+        ///g.drawLine(500,600,500,500);
+        //for(int i = 0; i<10; i++){
+        DrawFractal(g,700,700,100,angle1);
+        //}
     }
+
+    public void DrawFractal(Graphics g, int x, int y, int prevLength, double prevAngle){
+        int length = prevLength +decrease;
+        if(length <=50){
+            int bifhae = 1;
+        }else{
+            double rightAngle = prevAngle-angle1;
+            double leftAngle = prevAngle+angle1;
+            int rightx = x-(int)(length*Math.sin(rightAngle));
+            int righty = y-(int)(length*Math.cos(rightAngle));
+            int leftx = x-(int)(length*Math.sin(leftAngle));
+            int lefty =y-(int)(length*Math.cos(leftAngle));
+            
+            if(count !=0){
+                DrawFractal(g,leftx,lefty,length,leftAngle);
+                g.drawLine(x,y,leftx,lefty);
+            }else{count =4;}
+            g.drawLine(x,y,rightx,righty);
+            DrawFractal(g,rightx,righty,length,rightAngle);
+            
+        }
     }
-    
-    public void angleCalc(Graphics g,double angle){
-        length = length +decrease;
-        newx = oldx-(int)(length*Math.sin(angle));
-        newy = oldy-(int)(length*Math.cos(angle));
-        g.drawLine(oldx,oldy, newx,newy);
-        oldx = newx;
-        oldy = newy;
-    }
-    
+
+//     public void angleCalc(Graphics g,double angle){
+//         length = length +decrease;
+//         newx = oldx-(int)(length*Math.sin(angle));
+//         newy = oldy-(int)(length*Math.cos(angle));
+//         g.drawLine(oldx,oldy, newx,newy);
+//         oldx = newx;
+//         oldy = newy;
+//     }
+
 }
